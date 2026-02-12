d1fc06591b78438f432640c4d13858d2f978b530
Tomek <t.pielczyk@netzindianer.net>
2026-02-12 13:08:15 +0100

    add initial configuration and documentation for Levada Analytics

--- docs/.vitepress/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  title: "Levada Analytics",
-  base: "/levada-analytics/"
-}
---
+++ src/.vitepress/config.js
@@ -0,0 +1,5 @@
+export default {
+  title: "Levada Analytics",
+  base: "/levada-analytics/",
+  outDir: "../docs",
+}
